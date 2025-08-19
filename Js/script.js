c9c5100876b8fe301754cee9e182dd7735a567f0
Heiress-b <uwaziesuccess02@gmail.com>
2025-08-19 09:52:49 -0700

    Mondilla connects Landing Page

---
+++ Js/script.js
@@ -0,0 +1,30 @@
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  // Save preference in localStorage
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
+// Load saved theme on page load
+window.addEventListener("load", () => {
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+  }
+});
+
+const toggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("mobile-menu");
+toggle.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
+
+document.getElementById("contact-form").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Message submitted! (Demo only)");
+});
